b70461e30fd4fda6365b44b95881cef7cb006544
evanq123 <evanq123@gmail.com>
2018-01-20 14:19:47 -0500

    [v1] Release

---
+++ PushBinance.py
@@ -0,0 +1,55 @@
+import time
+import datetime
+import sys
+try:
+    from bs4 import BeautifulSoup
+    from pushbullet import PushBullet
+    from binance.client import Client
+except ImportError:
+    print("Requirements were not installed. Please consult guide at\n"
+          "https://github.com/evanq123/PushBinance \n")
+    sys.exit(1)
+
+api_key = input("Enter your binance api key: ")
+api_secret = input("Enter your binance api secret: ")
+client = Client(api_key, api_secret)
+push_key = input("Enter your PushBullet api key: ")
+symbol = input("Enter the symbol: ").upper() + 'BTC'
+threshold = float(input("Enter your threshold in BTC: "))
+intervals = 5 # Default 5s between checks.
+
+def get_price():
+    price = client.get_symbol_ticker(symbol=symbol)["price"]
+    print("Price for {} is now: {} BTC".format(symbol, price))
+    return price
+
+
+def push_message(msg):
+    pb = PushBullet(push_key)
+    pb.push_note("PushBinance: The price for {} has changed!".format(symbol), msg)
+
+
+message_sent = False # Hackish workaround for now.
+def price_past_threshold():
+    if float(get_price()) >= float(threshold):
+        return True
+
+
+while True:
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if price_past_threshold() and message_sent is False:
+        msg = ("As of, {} EST, the price for {} is >= {} BTC (at {})"
+               "".format(date, symbol, threshold, get_rate(currency)))
+
+        print("Sending message to PushBullet...\n")
+        push_message(msg)
+        message_sent = True
+
+    if not price_past_threshold() and message_sent is True:
+        msg = ("As of, {} EST, the rate for {} is < {} BTC (at {})"
+               "".format(date, symbol, threshold, get_rate(currency)))
+        print("Sending message to PushBullet...\n")
+        push_message(msg)
+        message_sent = False
+
+    time.sleep(intervals)
